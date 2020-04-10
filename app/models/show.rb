4599b0e1c859a406ce0cae1b866c5afec6ff7344
Timur Mishiev <timurmishiev@gmail.com>
2020-04-10 01:58:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,8 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network
   
-  def actors_list
-    
-  end 
+  self.characters.map do |c|
+            "#{c.actor.full_name}"
+        end
+    end
 end
